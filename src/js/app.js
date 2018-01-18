da50c08331b68c42207ccd2f5c5b5a70ec1fa457
smosshor <smosshorwitz@gmail.com>
2018-01-18 13:34:52 -0500

    /

---
+++ src/js/app.js
@@ -0,0 +1,42 @@
+/*Needed steps: 
+- User clicks a given card  
+- load content based on url and save into variable
+- 
+
+*/
+
+$(document).ready(function () {
+
+    $('.portfolioItem1').css("background", red);
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.portfolioItem1').css("background", red);
+        }
+    });
+});
+
+
+
+/*Function to control clicking on a card
+    $('#cards a').click(function () {
+        e.preventDefault();
+        
+        url(/Users/Sarah/Documents/GitHub/portfolio_2018/src/img/bg2.jpg)
+
+        // save content to load into a variable -- #content-- 
+        var toLoad = $(this).attr('href') + '#content';
+        loadContent(toLoad);
+
+        //Load content, contains showNewContent
+        function loadContent() {
+            $('#contentSection').load(toLoad);
+        }
+    });
+    // Function to control clicking OUT of a card
+
+    $('#itemx a').click(function () {
+        //hide the content on the currrent page 
+        $('#content').hide('fast', loadContent);
+    });
+    */
